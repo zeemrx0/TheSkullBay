cfba08f3e0247fddbf393e57e99c5242570978c1
Duy Huỳnh Quốc <duyhq.zee@gmail.com>
2024-03-24 19:07:30 +0700

    Save

--- Assets/Scripts/Inventories/PlayerInventory/PlayerInventoryPresenter.cs
+++ Assets/Scripts/Inventories/PlayerInventory/PlayerInventoryPresenter.cs
@@ -1,9 +1,32 @@
-using UnityEngine;
+using LNE.Utilities.Constants;
 
 namespace LNE.Inventories
 {
   public class PlayerInventoryPresenter : InventoryPresenter
   {
-    private void SaveToFile() { }
+    private void SaveToFile()
+    {
+      ES3.Save<InventoryModel>(
+        SavingKey.PlayerInventoryKey,
+        value: _model,
+        SavingKey.PlayerInventoryPath
+      );
+    }
+
+    private void LoadFromFile()
+    {
+      if (
+        ES3.KeyExists(
+          SavingKey.PlayerInventoryKey,
+          SavingKey.PlayerInventoryPath
+        )
+      )
+      {
+        _model = ES3.Load<InventoryModel>(
+          SavingKey.PlayerInventoryKey,
+          SavingKey.PlayerInventoryPath
+        );
+      }
+    }
   }
 }
